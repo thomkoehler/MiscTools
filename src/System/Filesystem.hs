5db512ad2e1644f5cf543fae3525e7b4009907d7
thomkoehler <thom.koehler@gmail.com>
2013-10-26 13:57:16 +0200

    add textFinfFile add eclipse project

--- src/System/Filesystem.hs
+++ src/System/Filesystem.hs
@@ -3,13 +3,19 @@
 
 module System.Filesystem
 (
-   listDir
+   listDir,
+   textFindInFile
 ) where
 
 
+import qualified Data.ByteString as B
+import Control.Monad.Trans.Resource(runResourceT, allocate, release)
+import Control.Monad.Trans.Class(lift)
+import Data.ByteString.Search(breakOn)
 import System.Directory(getDirectoryContents, doesDirectoryExist)
 import System.FilePath((</>), takeFileName)
 import Control.Monad(forM)
+import System.IO(openFile, hClose, IOMode(ReadMode), hIsEOF, hSeek, SeekMode(RelativeSeek))
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -28,6 +34,33 @@ listDir topdir fileSelect recursive = do
                else return []
    return (concat paths)
 
+
+textFindInFile :: B.ByteString -> Int -> FilePath -> IO Bool 
+textFindInFile text bufferSize fileName = runResourceT $ do
+   let
+      textSize = B.length text
+      propBufferSize = ((bufferSize `div` textSize) + 2) * textSize
+      
+   (fileReleaser, handle) <- allocate (openFile fileName ReadMode) hClose
+   res <- lift $ find handle textSize propBufferSize
+   release fileReleaser 
+   return res
+   
+      where
+         find handle textSize propBufferSize = do
+            buffer <- B.hGetSome handle propBufferSize
+            let (_, res) = breakOn text buffer
+            if B.null res
+               then do
+                  isEof <- hIsEOF handle
+                  if isEof
+                     then return False
+                     else do
+                        hSeek handle RelativeSeek $ toInteger (- textSize)
+                        find handle textSize propBufferSize
+               else
+                  return True
+                  
 -----------------------------------------------------------------------------------------------------------------------
 
 
