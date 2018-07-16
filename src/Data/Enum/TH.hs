6c85aae5312536046e1364b807aa609b6e55b5d0
thomkoehler <thom.koehler@gmail.com>
2018-07-16 12:22:28 +0200

    add stack support, bug fixing

--- src/Data/Enum/TH.hs
+++ src/Data/Enum/TH.hs
@@ -14,10 +14,9 @@ import Language.Haskell.TH
 enumADT :: String -> [(String, Integer)] -> Q [Dec]
 enumADT typeNameS elems = do
   let typeName = mkName typeNameS
-  let dataDecl = DataD [] typeName [] (map ((`NormalC` []) . mkName . fst) elems) [''Eq, ''Ord, ''Show]
-  let toEnumMethods = (FunD (mkName "toEnum") (map toEnumClauses elems))
-  let fromEnumMethods = (FunD (mkName "fromEnum") (map fromEnumClauses elems))
-  let enumInstance = InstanceD [] (AppT (ConT (mkName "Enum")) (ConT typeName)) [toEnumMethods, fromEnumMethods]
+  let dataDecl = DataD [] typeName [] Nothing (map ((`NormalC` []) . mkName . fst) elems) [DerivClause Nothing [ConT ''Eq, ConT ''Ord, ConT ''Show]]
+  let toEnumMethods = FunD (mkName "toEnum") (map toEnumClauses elems)
+  let enumInstance = InstanceD Nothing [] (AppT (ConT (mkName "Enum")) (ConT typeName)) [toEnumMethods, fromEnumMethods elems]
   return [dataDecl, enumInstance]
   where
       toEnumClauses (name, value) = Clause [LitP (IntegerL value)] (NormalB (ConE (mkName name))) []
@@ -29,11 +28,16 @@ enumADTDef typeNameS defFunctionS elems = do
   let defFunction = mkName defFunctionS
   let typeName = mkName typeNameS
   let xName = mkName "x"
-  let dataDecl = DataD [] typeName [] (map ((`NormalC` []) . mkName . fst) elems) [''Eq, ''Ord, ''Show]
-  let toEnumMethods = (FunD (mkName "toEnum") ((map toEnumClauses elems) ++ [Clause [VarP xName] (NormalB (AppE (VarE defFunction) (VarE xName))) []]))
-  let fromEnumMethods = (FunD (mkName "fromEnum") (map fromEnumClauses elems))
-  let enumInstance = InstanceD [] (AppT (ConT (mkName "Enum")) (ConT typeName)) [toEnumMethods, fromEnumMethods]
+  let dataDecl = DataD [] typeName [] Nothing (map ((`NormalC` []) . mkName . fst) elems) [DerivClause Nothing [ConT ''Eq, ConT ''Ord, ConT ''Show]]
+  let toEnumMethods = FunD (mkName "toEnum") (map toEnumClauses elems ++ [Clause [VarP xName] (NormalB (AppE (VarE defFunction) (VarE xName))) []])
+  let enumInstance = InstanceD Nothing [] (AppT (ConT (mkName "Enum")) (ConT typeName)) [toEnumMethods, fromEnumMethods elems]
   return [dataDecl, enumInstance]
   where
       toEnumClauses (name, value) = Clause [LitP (IntegerL value)] (NormalB (ConE (mkName name))) []
       fromEnumClauses (name, value) = Clause [ConP (mkName name) []] (NormalB (LitE (IntegerL value))) []
+
+      
+fromEnumMethods :: [(String, Integer)] -> Dec
+fromEnumMethods elems = FunD (mkName "fromEnum") (map fromEnumClauses elems)
+  where
+    fromEnumClauses (name, value) = Clause [ConP (mkName name) []] (NormalB (LitE (IntegerL value))) []
